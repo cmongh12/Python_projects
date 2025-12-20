18491b777d679ae0ba55b24b80da4824f3e3bdca
cmongh12 <cmongh12@gmail.com>
2025-12-20 12:58:08 +0545

    python

---
+++ nested.py
@@ -0,0 +1,84 @@
+# x=4
+# y=5
+
+# if x>y:
+#     print("x is greater than y")
+# elif x==y:
+#     print("x is equal to y")
+# else:
+#     print("x is not greater than y")
+
+# x=6
+# y=7
+# z=2
+
+# if x>y and x>z:
+#    print ("x is greater than y and z") 
+# elif y>x and y>z:
+#    print("y is greater than x and z")
+# else:
+#    print("z is greater than x and y")
+
+# x=60
+# y=70
+# z=20
+# a=50
+
+# if x>y and x>z and x>a:
+#     print ("x is the greatest")
+# elif y>x and y>z and y>a:
+#     print("y is the greatest")
+# elif z>x and z>y and z>a:
+#     print("z is the greatest")
+# else:
+#     print("a is the greatest")
+
+# username= "admin"
+# password="admin123"
+# if username=="admin" and password=="admin123":
+#     print("login successful")
+# elif username=="hari" and password=="hari123":
+#     print("login successful")
+# elif username=="john" and password=="john123":
+#     print("login successful")
+# else:
+#     print("login unsuccessful")
+
+# WAP to enter age of person and check whether the person is eligible to vote or not.
+# is eligible to vote or not 18<40.
+
+# age=int(input("Enter your age: "))
+
+# if age>=18  and age<=40:
+#     print("you are eligible to vote")
+# elif age<18:
+#    print("you are underage to vote",age)
+# else:
+#     print("you are a senior citizen",age)
+
+# party (age 18<40>)
+# drinks >18 25< fanta
+# 25<35 beer
+# 35<40 whiskey
+
+# match case
+
+# age=int(input("Enyer your age: "))
+
+# if age>=18 and age<40:
+#     print("You are eligible to enter the party")
+#     if age>18 and age<25:
+#         print("You can drink fanta")
+#     else:
+#        if age>=25 and age<35:
+#             print("You can drink beer")
+#        else:
+#             if age>=35 and age<40:
+#                 print("you can drink whiskey")
+#             else:
+#                 print("You are not allowed to drink")
+
+# else:
+#     print("You are not allowed to enter the party")
+            
+
