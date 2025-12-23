13c883dfdc15eea7bc77407ff086a313e4705d34
cmongh12 <cmongh12@gmail.com>
2025-12-23 18:58:39 +0545

    python

---
+++ loop.py
@@ -0,0 +1,62 @@
+# n=1
+
+# while n<=5:
+#     print(n)
+#     n+=1
+
+# n=5
+
+# while n>=1:
+#     print(n)
+#     n-=1
+
+# n=1
+# while n<=10:
+#     print("simon")
+#     n+=1
+
+# n=1
+
+# while n<=10:
+#     if n%2==0:
+#         print(n)
+#     n+=1
+
+# n=1
+# while n<=10:
+#     if n%2!=0:
+#         print(n)
+#     n+=1
+
+# n=1
+# total=0
+
+# while n<=10:
+#     total+=n
+#     n+=1
+
+# print(total)
+
+# n=1
+# even=0
+# odd=0
+
+# while n<=25:
+#     if n%2==0:
+#         even+=1
+#     else:
+#         odd+=1
+#     n+=1
+
+#     print("Total even number is:",even)
+#     print("Total odd number is:",odd)
+
+# n=1
+# while n<=10:
+#     if n<=5:
+#         print("Ram")
+#         n+=1
+#     else:
+#         print("sita")
+#         n+=1
+
