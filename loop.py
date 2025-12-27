26445669e26d27cd78ea32d63743943d1a2b2cfd
cmongh12 <cmongh12@gmail.com>
2025-12-28 04:27:39 +0545

    python

--- loop.py
+++ loop.py
@@ -60,3 +60,83 @@
 #         print("sita")
 #         n+=1
 
+# WAP to enter number of students : 5
+# enter name of student
+
+# a = int(input("Enter number of students: "))
+
+# i=1
+# while i<=a:
+#     name =  input("Enter name of student: ")
+#     print("Student name:",name)
+#     i+=1
+
+# print(range(10))
+
+# for x in range(1,11):
+#     # print(x)
+#     print(x,end=" ")
+
+# for x in range(10,0,-1):
+#     print(x,end=" ")
+
+# data=['ram','hari','gita','sita']
+# for name in data:
+#     print(name)
+
+# numbers=[1,3,6,7,9,11,12,15,17]
+# # for n in numbers:
+# #     if n%2==0:
+# #         print(n)
+
+# for n in numbers:
+#     if n==3 or n==9 or n==15 or n==17:
+#         print(n)
+
+# name=['ram','sita']
+# n=1
+# for n in range(1,16):
+#     if n<=5:
+#         print("ram")
+#     elif n>5 and n<=10:
+#         print("gita")
+#     else:
+#         print("sita")
+
+# for n in range(1,11):
+#     print("5x",n,"=",n*5)
+    # print(f"5x{n}={5*n}")
+
+
+# total_users=?
+# total_male=?
+# total_female=?
+
+# data=[
+#     {'name':'ram','gender':'male'},
+#     {'name':'sita','gender':'female'}, 
+#     {'name':'hari','gender':'male'},  
+#     {'name':'gita','gender':'female'} 
+# ]
+
+# total_users = 0
+# total_male = 0
+# total_female = 0
+
+# for users in data:
+#     total_users += 1
+
+#     if users['gender'] == 'male':
+#         total_male += 1
+#     elif users['gender'] == 'female':
+#         total_female += 1
+
+# print("Total_users:", total_users)
+# print("Total_male:", total_male)
+# print("Total_female:", total_female)
+
+
+
+
+
+
