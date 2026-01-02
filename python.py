345a16074fb9e7506cded13ddaeb2f8525cb7531
cmon ghale <cmonghale@cmons-MacBook-Air.local>
2026-01-02 21:52:06 +0545

    python

---
+++ python.py
@@ -0,0 +1,112 @@
+# print("hello pyhton")
+
+# output to be:
+#  *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+# output to be:
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+# for i in range(6,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+# output to be:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5 
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# output to be:
+# 0
+# 0 1
+# 0 1 2 
+# 0 1 2 3 
+# 0 1 2 3 4
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(j,end="")
+#     print()
+
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(j,end="")
+#     print()
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print(j,end="")
+#     print()
+
+# def total(x,y):
+#     return x+y
+
+# def my_total(a,b,cb):
+#     print(cb(a+b))
+
+# my_total (5,6,total)
+
+# def cal(a,b):
+#         print(a+b)
+
+# cal(4,6)
+
+# even_or_odd
+
+# def num(n):
+#     if n%2==0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# num(4)
+
+# a,b,c
+
+# def greater(a,b,c):
+#     if a>b and a>c:
+#         print("a is greater")
+#     elif b>a and b>c:
+#         print("b is greater")
+#     else:
+#         print("c is greater")
+
+# greater(4,7,8)
+
+#  ascending. number lai pass garne , descending order ma ni pass garne, loop chalayera sum nikalna try garne
+
+# def sort_ascending (numbers):
+#     return sorted (numbers)
+
+# nums=[8,6,7,5,2,1]
+
+# ascending_result= sort_ascending(nums)
+# print("ascending_order:",ascending_result)
+
+# def sort_descending (numbers):
+#     return sorted (numbers)
+
+# nums=[9,8,5,3,4,2]
+
+# descending_results= sort_descending(nums)
+# print("descending_order:",descending_results)
+
